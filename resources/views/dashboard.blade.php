73a8cc6f3f6c065b0e0703ddda758786b75a8efa
sabin3230 <saveen.man3230@gmail.com>
2022-10-31 23:08:02 +0545

    authentication assignment

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+
+@extends('main')
+
+@section('content')
+
+<div class="card">
+	<div class="card-header">Dashboard</div>
+	<div class="card-body">
+		
+        hello , welcome to Ekbana company
+	</div>
+</div>
+
+@endsection('content')
